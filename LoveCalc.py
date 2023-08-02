806b32df20850edc2537de308536ea451b59cc69
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-08-02 15:37:15 +0100

    Corrected Operators in if statement

--- LoveCalc.py
+++ LoveCalc.py
@@ -29,7 +29,7 @@ print(love_score)
 
 if love_score <= 10 or love_score >= 90:
     print(f"Your score is {love_score}, you go together like coke and mentos")
-elif love_score >= 40 or love_score <= 50:
+elif love_score >= 40 and love_score <= 50:
     print(f"your score is {love_score}, you are alright together.")
 else:
     print(f"Your score is {love_score}")
